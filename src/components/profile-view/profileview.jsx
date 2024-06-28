6c555925529ec22becc630587861f8da664797e8
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-06-27 20:39:57 -0400

    Update profileview.jsx

--- src/components/profile-view/profileview.jsx
+++ src/components/profile-view/profileview.jsx
@@ -1,191 +1,138 @@
-import React, { useState, useEffect } from "react";
-import { Form, Button, Row, Col, Card } from "react-bootstrap";
-import { MovieCard } from "../movie-card/movie-card";
+import React, { useState, useEffect } from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+import { ProfileView } from '../profile-view/profileview';
+import { AppNavbar } from '../navigation-bar/navigationbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const ProfileView = ({ user, token, onUserUpdate }) => {
-  const [username, setUsername] = useState(user.Username);
-  const [email, setEmail] = useState(user.Email);
-  const [birthday, setBirthday] = useState(user.Birthday);
-  const [password, setPassword] = useState("");
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
+import Row from "react-bootstrap/Row";
+import Col from 'react-bootstrap/Col';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-  useEffect(() => {
-    fetch(`https://movie-api-3jxi.onrender.com/users/${user.Username}/movies`, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setFavoriteMovies(data);
-      });
-  }, [token, user.Username]);
-
-  const handleUpdate = (event) => {
-    event.preventDefault();
-
-    const data = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday,
-    };
-
-    fetch(`https://movie-api-3jxi.onrender.com/users/${user.Username}`, {
-      method: "PUT",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Update failed");
-        }
-      })
-      .then((updatedUser) => {
-        alert("Profile updated successfully");
-        onUserUpdate(updatedUser);
-      })
-      .catch((error) => {
-        console.error("Error updating profile:", error);
-        alert("An error occurred. Please try again.");
-      });
-  };
-
-  const handleRemoveFavorite = (movieId) => {
-    fetch(`https://movie-api-3jxi.onrender.com/users/${user.Username}/movies/${movieId}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          setFavoriteMovies((movies) => movies.filter((movie) => movie._id !== movieId));
-        } else {
-          throw new Error("Failed to remove movie from favorites");
-        }
-      })
-      .catch((error) => {
-        console.error("Error removing favorite movie:", error);
-        alert("An error occurred. Please try again.");
-      });
-  };
-
-
-
-  const handleDeregister = () => {
-    fetch(`https://movie-api-3jxi.onrender.com/users/${user.Username}`, {
-      method: "DELETE",
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((response) => {
-        if (response.ok) {
-          alert("Account deleted successfully");
-          localStorage.clear();
-          onUserUpdate(null); // You might want to redirect to the login or home page here
-        } else {
-          throw new Error("Failed to delete account");
-        }
-      })
-      .catch((error) => {
-        console.error("Error deleting account:", error);
-        alert("An error occurred. Please try again.");
-      });
-  };
-
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
 
   useEffect(() => {
-    fetch(`https://movie-api-3jxi.onrender.com/movies`, {
-      headers: { Authorization: `Bearer ${token}` },
+    if (!token) {
+      return;
+    }
+    fetch("https://movie-api-3jxi.onrender.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
     })
-      .then((response) => response.json())
-      .then((data) => {
-        const favoriteMovies = data.filter((movie) => user.FavoriteMovies.includes(movie._id));
-        setFavoriteMovies(favoriteMovies);
-      });
-  }, [token, user.Username, user.FavoriteMovies]);
-  
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch movies');
+      }
+    })
+    .then((data) => {
+      setMovies(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching movies:', error);
+    });
+  }, [token]);
 
+  const onLoggedIn = (user, token) => {
+    setUser(user);
+    setToken(token);
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+  };
 
+  const onUserUpdate = (updatedUser) => {
+    setUser(updatedUser);
+    localStorage.setItem("user", JSON.stringify(updatedUser));
+  };
 
-
-  <Button variant="danger" onClick={handleDeregister}>
-  Delete Account
-</Button>
-
+  const onLogout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.clear();
+  };
 
   return (
-    <Row>
-      <Col md={6}>
-        <Form onSubmit={handleUpdate}>
-          <Form.Group controlId="formUsername">
-            <Form.Label>Username:</Form.Label>
-            <Form.Control
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              minLength="3"
-            />
-          </Form.Group>
-
-          <Form.Group controlId="formPassword">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </Form.Group>
-
-          <Form.Group controlId="formEmail">
-            <Form.Label>Email:</Form.Label>
-            <Form.Control
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </Form.Group>
-
-          <Form.Group controlId="formBirthday">
-            <Form.Label>Birthday:</Form.Label>
-            <Form.Control
-              type="date"
-              value={birthday}
-              onChange={(e) => setBirthday(e.target.value)}
-              required
-            />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Update Profile
-          </Button>
-        </Form>
-      </Col>
-      <Col md={6}>
-        <h3>Favorite Movies</h3>
-        {favoriteMovies.length === 0 ? (
-          <p>No favorite movies</p>
-        ) : (
-          favoriteMovies.map((movie) => (
-            <Card key={movie._id} className="mb-3">
-              <Card.Body>
-                <MovieCard movie={movie} />
-                <Button
-                  variant="danger"
-                  onClick={() => handleRemoveFavorite(movie._id)}>
-                  Remove from Favorites
-                </Button>
-              </Card.Body>
-            </Card>
-          ))
-        )}
-      </Col>
-    </Row>
+    <BrowserRouter>
+      <AppNavbar user={user} onLogout={onLogout} />
+      <Row className="justify-content-md-center">
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              user ? (
+                <Navigate to="/" />
+              ) : (
+                <Col md={5}>
+                  <SignupView />
+                </Col>
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              user ? (
+                <Navigate to="/" />
+              ) : (
+                <Col md={5}>
+                  <LoginView onLoggedIn={onLoggedIn} />
+                </Col>
+              )
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              user ? (
+                <Col md={8}>
+                  <ProfileView user={user} token={token} onUserUpdate={onUserUpdate} />
+                </Col>
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+          <Route
+            path="/movies/:movieId"
+            element={
+              user ? (
+                <Col md={8}>
+                  <MovieView movies={movies} />
+                </Col>
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+          <Route
+            path="/"
+            element={
+              user ? (
+                movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <>
+                    {movies.map((movie) => (
+                      <Col className="mb-5" key={movie._id} md={3}>
+                        <MovieCard movie={movie} />
+                      </Col>
+                    ))}
+                  </>
+                )
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+        </Routes>
+      </Row>
+    </BrowserRouter>
   );
 };
